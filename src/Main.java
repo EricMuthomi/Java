96570fb0519584b6700538d8784dda9bf81254dc
Eric Palmer <muthomiericpalmer@gmail.com>
2023-01-13 17:27:21 +0300

    First start

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("My first Java program from scratch!");
+    }
+}
+
+/*Every java program must have a class and a main method
+* The main method executes tasks of the program
+* Java comments are usually ignored by the compiler.
+* They add important context for the human reader to understand the code
+* Java is a compiled language. The compiler catches errors and mistakes in our code
+* Compilers then transform the code into an executable class*/
